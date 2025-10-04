6f9e59dfaa8ef682e3aa292e3fe50dd572292449
Sergey Sokolkin <hawkxdev@gmail.com>
2025-10-04 21:14:07 +0300

    chore: update ALLOWED_HOSTS to include localhost IP in settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '84.252.138.183', 'kittygramxyz.duckdns.org']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '84.252.138.183', 'kittygramxyz.duckdns.org']
 
 INSTALLED_APPS = [
     'api.apps.ApiConfig',
